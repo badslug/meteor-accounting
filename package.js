330cf532e6ac7282cf85ef3b83a54c1a25f5bdae
Iain Shigeoka <iain@taptopia.com>
2012-08-31 11:41:14 -0700

    Fix broken package spec

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.add_files([
-        "accounting.js/accounting.js"
+        "accounting.js"
     ]
         , ['client', 'server']);
 });
